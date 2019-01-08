342e7f7e02ddae490b731e055dbadcf710259027
Carver Harrison <ch@dorper.me>
2019-01-07 19:27:46 -0800

    q

--- src/org/usfirst/frc/team5937/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5937/robot/subsystems/Drivetrain.java
@@ -1,18 +1,9 @@
 package org.usfirst.frc.team5937.robot.subsystems;
 
-import org.usfirst.frc.team5937.robot.devices.Wheel;
-
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
 
 // Abstract class for a drive train. Drive train classes should extend this.
 public abstract class Drivetrain extends Subsystem {
-
-	private Wheel frontRight;
-	private Wheel frontLeft;
-	private Wheel backRight;
-	private Wheel backLeft;
 	
 	public double speed = 1.0;
 	
@@ -50,16 +41,5 @@ public abstract class Drivetrain extends Subsystem {
 	}
 	
 	public void stop() {
-		frontRight.stop();
-		frontLeft.stop();
-		backRight.stop();
-		backLeft.stop();
-	}
-	
-	public void doMotion(double fr, double fl, double br, double bl) {
-		frontRight.set(fr*this.speed);
-		frontLeft.set(fl*this.speed);
-		backRight.set(br*this.speed);
-		backLeft.set(bl*this.speed);
 	}
 }
